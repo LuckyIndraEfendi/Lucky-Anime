73312fc5461da646d60d69e6e65c091545e09c38
LuckyIndraEfendi <kimilbonchu@gmail.com>
2022-06-24 22:36:51 +0700

    change file

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
- import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-analytics.js";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
+//  // Import the functions you need from the SDKs you need
+//  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
+//  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-analytics.js";
+//  // TODO: Add SDKs for Firebase products that you want to use
+//  // https://firebase.google.com/docs/web/setup#available-libraries
 
- // Your web app's Firebase configuration
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- const firebaseConfig = {
-   apiKey: "AIzaSyCpMARYWgEyAj83QhRR4Xmz9ZJdWKYCd78",
-   authDomain: "lucky-anime.firebaseapp.com",
-   projectId: "lucky-anime",
-   storageBucket: "lucky-anime.appspot.com",
-   messagingSenderId: "717979885068",
-   appId: "1:717979885068:web:7c6c6684783ee98cd2d829",
-   measurementId: "G-G5R8L5PZHJ"
- };
+//  // Your web app's Firebase configuration
+//  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//  const firebaseConfig = {
+//    apiKey: "AIzaSyCpMARYWgEyAj83QhRR4Xmz9ZJdWKYCd78",
+//    authDomain: "lucky-anime.firebaseapp.com",
+//    projectId: "lucky-anime",
+//    storageBucket: "lucky-anime.appspot.com",
+//    messagingSenderId: "717979885068",
+//    appId: "1:717979885068:web:7c6c6684783ee98cd2d829",
+//    measurementId: "G-G5R8L5PZHJ"
+//  };
 
- // Initialize Firebase
- const app = initializeApp(firebaseConfig);
- const analytics = getAnalytics(app);
+//  // Initialize Firebase
+//  const app = initializeApp(firebaseConfig);
+//  const analytics = getAnalytics(app);
